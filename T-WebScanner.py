8d05c8431b747845489f5a8cfaf48c2553d6ac53
Phan Than. Truoc <truoc.phan@techlabcorp.local>
2020-07-18 23:09:11 +0700

    xxxx

--- T-WebScanner.py
+++ T-WebScanner.py
@@ -29,8 +29,8 @@ def banner():
 =========================================================================
 =========================================================================
 ''')
-	print(Fore.GREEN + ' [+] Operating System: ' + platform.system() + ' ' + platform.release() + ' (Version: ' + platform.version() + ')' if platform.system() in ['Windows', 'Linux'] else Fore.RED + ' [-] Operating System: macOS ' + platform.release() + ' (Version: ' + platform.version() + ')')
-	print(Fore.GREEN + ' [+] Python Version: ' + platform.python_version() if platform.python_version()[0] == '3' else Fore.RED + ' [-] Python Version: ' + platform.python_version())
+	print(Fore.GREEN + ' [+] Operating System: ' + platform.system() + ' ' + platform.release() + ' (Version: ' + platform.version() + ')' if platform.system() in ['Windows', 'Linux'] else Fore.RED + ' [-] Operating System: ' + Style.RESET_ALL + Back.RED + 'macOS ' + Style.RESET_ALL + Fore.RED + platform.release() + ' (Version: ' + platform.version() + ')')
+	print(Fore.GREEN + ' [+] Python Version: ' + platform.python_version() if platform.python_version()[0] == '3' else Fore.RED + ' [-] Python Version: ' + Style.RESET_ALL + Back.RED + platform.python_version())
 	print('')
 
 def main():
@@ -42,4 +42,4 @@ if __name__ == '__main__':
 	if OS in ['Windows', 'Linux'] and PythonVersion[0] == '3':
 		main()
 	else:
-		exit(" Please use " + Back.RED + "python 3.x.x" + Style.RESET_ALL + " to run T-WebScanner and run on " + Back.RED + "Window" + Style.RESET_ALL + " or " + Back.RED + "Linux" + Style.RESET_ALL)
+		exit(Fore.RED + " Please use " + Style.RESET_ALL + Back.GREEN + "python 3.x.x" + Style.RESET_ALL + Fore.RED + " to run T-WebScanner and run on " + Style.RESET_ALL + Back.GREEN + "Windows" + Style.RESET_ALL + Fore.RED + " or " + Style.RESET_ALL + Back.GREEN + "Linux" + Style.RESET_ALL + "\n")
